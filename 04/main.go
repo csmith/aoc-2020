eb05db1d5f7ab1f3999ccc5913a53b480ffca058
Chris Smith <chris@chameth.com>
2020-12-04 15:19:11 +0000

    Ugly day 4

---
+++ 04/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/csmith/aoc-2020/common"
+	"regexp"
+	"strings"
+)
+
+func parse(lines []string) []map[string]string {
+	var passports []map[string]string
+	details := make(map[string]string)
+	for i := range lines {
+		if strings.TrimSpace(lines[i]) == "" {
+			passports = append(passports, details)
+			details = make(map[string]string)
+		} else {
+			words := strings.Split(lines[i], " ")
+			for j := range words {
+				parts := strings.SplitN(words[j], ":", 2)
+				details[parts[0]] = parts[1]
+			}
+		}
+	}
+	passports = append(passports, details)
+	return passports
+}
+
+func main() {
+	lines := common.ReadFileAsStrings("04/input.txt")
+
+	passports := parse(lines)
+	rules := map[string]*regexp.Regexp{
+		"byr": regexp.MustCompile(`^(19[0-9]{2}|200[012])$`),
+		"iyr": regexp.MustCompile(`^20(1\d|20)$`),
+		"eyr": regexp.MustCompile(`^20(2\d|30)$`),
+		"hgt": regexp.MustCompile(`^(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)$`),
+		"hcl": regexp.MustCompile(`^#[\da-f]{6}$`),
+		"ecl": regexp.MustCompile(`^(amb|blu|brn|gry|grn|hzl|oth)$`),
+		"pid": regexp.MustCompile(`^\d{9}$`),
+	}
+
+	presentCount := 0
+	validatedCount := 0
+	for i := range passports {
+		present := true
+		validated := true
+		for field, regex := range rules {
+			val, ok := passports[i][field]
+			if !ok {
+				present = false
+				break
+			}
+			if !regex.MatchString(val) {
+				validated = false
+			}
+		}
+		if present {
+			presentCount++
+			if validated {
+				validatedCount++
+			}
+		}
+	}
+
+	println(presentCount)
+	println(validatedCount)
+}
